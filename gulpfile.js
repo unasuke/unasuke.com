bbc120b345ff1f8e404139b23916be54ad03c14f
Yusuke Nakamura <yusuke1994525@gmail.com>
2018-09-19 01:03:24 +0900

    Fix wrong path in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,21 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 
 gulp.task('base.scss', () => {
-  return gulp.src('./source/stylesheets/source/base.scss')
+  return gulp.src('./source/stylesheets/src/base.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./source/stylesheets/base.css'));
+    .pipe(gulp.dest('./source/stylesheets/'));
 })
 
 gulp.task('index.scss', () => {
-  return gulp.src('./source/stylesheets/source/index.scss')
+  return gulp.src('./source/stylesheets/src/index.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./source/stylesheets/index.css'));
+    .pipe(gulp.dest('./source/stylesheets/'));
 })
 
 gulp.task('sponsor.scss', () => {
-  return gulp.src('./source/stylesheets/source/sponsor.scss')
+  return gulp.src('./source/stylesheets/src/sponsor.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./source/stylesheets/sponsor.css'));
+    .pipe(gulp.dest('./source/stylesheets/'));
 })
 
 gulp.task('build', ['base.scss', 'index.scss', 'sponsor.scss']);
