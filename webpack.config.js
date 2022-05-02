9ef509a3a03acb2a0bf37dc212961a9a2af58f70
Yusuke Nakamura <yusuke1994525@gmail.com>
2022-05-02 20:22:43 +0900

    from gulp to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,29 @@
-const path = require('path');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const RemoveEmptyScriptsPlugin = require("webpack-remove-empty-scripts");
 
 module.exports = {
   entry: {
-    'base': './source/stylesheets/src/base.scss',
-    'index': './source/stylesheets/src/index.scss',
-    'sponsor': './source/stylesheets/src/sponsor.scss',
+    base: "./source/stylesheets/src/base.scss",
+    index: "./source/stylesheets/src/index.scss",
+    sponsor: "./source/stylesheets/src/sponsor.scss",
+    activity: "./source/stylesheets/src/activity.scss",
+    aboutme: "./source/stylesheets/src/aboutme.scss",
   },
   output: {
-    filename: '[name].css',
-    path: path.resolve(__dirname, 'source/stylesheets'),
+    path: path.resolve(__dirname, "source/stylesheets/"),
   },
   module: {
     rules: [
       {
         test: /\.scss$/,
         exclude: /node_modules/,
-        use: ExtractTextPlugin.extract({
-          use: 'css-loader!sass-loader'
-        }),
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
     ],
   },
   plugins: [
-    new ExtractTextPlugin('[name].css'),
-  ]
+    new RemoveEmptyScriptsPlugin(),
+    new MiniCssExtractPlugin({ filename: "[name].css" }),
+  ],
 };
